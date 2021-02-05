7f804f8d280144ce7b5a54bc2e5e2f20d0a2d804
Cody Thaller <stevencthaller@gmail.com>
2021-02-05 11:47:32 -0800

    Added recommended extensions

---
+++ Algos/feb_5.js
@@ -0,0 +1,4 @@
+// Change. Change is inevitable (especially when breaking a twenty). Make generateCoinChange(cents). 
+// Accept a number of American cents, compute and print how to represent that amount with 
+// smallest number of coins. Common American coins are pennies (1 cent), nickels (5 cents), 
+// dimes (10 cents), quarters (25 cents), and a dollar (100 cents).
